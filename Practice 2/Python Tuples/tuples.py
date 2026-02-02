1766b602992f081c3defddd5556091b8ffdf9de9
Romik <konyrbairamazan@gmail.com>
2026-02-02 19:37:24 +0500

    tuple

---
+++ Practice 2/Python Tuples/tuples.py
@@ -0,0 +1,27 @@
+#Tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+
+#Allow Duplicates
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+
+#Tuple Length
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+
+#Loop Through a Tuple
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+
+#Join Two Tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
