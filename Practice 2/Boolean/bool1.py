549f69475eac32723e80a862a66fc478ebd6cfc4
Romik <konyrbairamazan@gmail.com>
2026-02-02 19:26:32 +0500

    boolean

---
+++ Practice 2/Boolean/bool1.py
@@ -0,0 +1,28 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+a = 100
+b = 15
+
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+print(bool("Hello"))
+print(bool(15))
+
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
