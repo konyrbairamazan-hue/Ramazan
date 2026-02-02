7d059f1885c169ca6900fcae49648debef071574
Romik <konyrbairamazan@gmail.com>
2026-02-02 19:33:29 +0500

    for

---
+++ Practice 2/Python For loops/for1.py
@@ -0,0 +1,26 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+for x in "banana":
+  print(x)
+
+
+#Exit the loop when x is "banana":
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+
+for x in range(6):
+  print(x)
