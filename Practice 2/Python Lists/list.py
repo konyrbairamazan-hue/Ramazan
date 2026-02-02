734231a5aa4253953111703bac4e40d7bb853cf4
Romik <konyrbairamazan@gmail.com>
2026-02-02 19:36:31 +0500

    list

---
+++ Practice 2/Python Lists/list.py
@@ -0,0 +1,24 @@
+#List
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+
+#List Length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
+#type()
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+
+#Sort List Alphanumerically
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
